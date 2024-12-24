4ff9f748809ac0d98fad9d1daa56748418096da3
H9draIrawan <hendra.i21@mhs.istts.ac.id>
2024-12-24 15:48:41 +0700

    add recommeded page

--- lib/ui/RecommendPage.dart
+++ lib/ui/RecommendPage.dart
@@ -5,24 +5,299 @@ class Recommendpage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        leading: IconButton(
-          icon: const Icon(Icons.arrow_back),
-          onPressed: () => Navigator.pop(context),
-        ),
-        title: const Text(
-          'RECOMMEND',
-          style: TextStyle(
-            fontSize: 16,
-            fontWeight: FontWeight.bold,
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text(
+            'Recommendation',
+            style: TextStyle(
+              color: Colors.black,
+              fontWeight: FontWeight.bold,
+            ),
           ),
         ),
-        backgroundColor: const Color(0xFF1A1A1A),
-        foregroundColor: Colors.white,
-      ),
-      body: const Center(
-        child: Text('Recommend Page'),
+        body: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(16.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                Row(
+                  children: [
+                    const Text(
+                      'Breakfast',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Spacer(),
+                    const Icon(
+                      Icons.local_fire_department,
+                    ),
+                    const Text(
+                      ' 450 kcal',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  elevation: 4,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Oatmeal Pisang',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Bahan-bahan:',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text('• 50 gram oatmeal'),
+                        const Text('• 1 buah pisang'),
+                        const Text('• 200 ml susu rendah lemak'),
+                        const Text('• 1 sendok makan madu'),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Cara mempersiapkan:',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Masak oatmeal dengan susu di atas api sedang, aduk hingga mendidih. Tambahkan pisang yang sudah dipotong dan madu sebelum disajikan.',
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Manfaat kesehatan:',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Memberikan energi yang baik dan serat tinggi untuk memulai hari.',
+                        ),
+                        const SizedBox(height: 16),
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Tambahkan logika untuk tombol "Add Plan"
+                            },
+                            child: const Text('ADD PLAN'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    const Text(
+                      'Lunch',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Spacer(),
+                    const Icon(
+                      Icons.local_fire_department,
+                    ),
+                    const Text(
+                      ' 600 kcal',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  elevation: 4,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Nasi Ayam Bakar',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Bahan-bahan:',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text('• 100 gram nasi'),
+                        const Text('• 100 gram ayam'),
+                        const Text('• 50 gram sayuran'),
+                        const Text('• 1 sendok makan sambal'),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Cara mempersiapkan:',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Masak nasi dan ayam, kemudian bakar ayam hingga matang. Sajikan dengan sayuran dan sambal.',
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Manfaat kesehatan:',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Mengandung protein tinggi dan karbohidrat kompleks yang baik untuk pertumbuhan anak.',
+                        ),
+                        const SizedBox(height: 16),
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Tambahkan logika untuk tombol "Add Plan"
+                            },
+                            child: const Text('ADD PLAN'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+                const SizedBox(height: 16),
+                Row(
+                  children: [
+                    const Text(
+                      'Dinner',
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    const Spacer(),
+                    const Icon(
+                      Icons.local_fire_department,
+                    ),
+                    const Text(
+                      ' 500 kcal',
+                      style: TextStyle(
+                        fontSize: 16,
+                        color: Colors.grey,
+                      ),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 16),
+                Card(
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  elevation: 4,
+                  child: Padding(
+                    padding: const EdgeInsets.all(16.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const Text(
+                          'Pasta Bolognese',
+                          style: TextStyle(
+                            fontSize: 20,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Bahan-bahan:',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text('• 100 gram pasta'),
+                        const Text('• 100 gram daging cincang'),
+                        const Text('• 50 gram saus tomat'),
+                        const Text('• 1 sendok makan keju parmesan'),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Cara mempersiapkan:',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Rebus pasta hingga matang, kemudian tumis daging cincang hingga matang. Tambahkan saus tomat dan keju parmesan.',
+                        ),
+                        const SizedBox(height: 16),
+                        const Text(
+                          'Manfaat kesehatan:',
+                          style: TextStyle(
+                            fontSize: 16,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        const SizedBox(height: 8),
+                        const Text(
+                          'Mengandung protein tinggi dan karbohidrat kompleks yang baik untuk pertumbuhan anak.',
+                        ),
+                        const SizedBox(height: 16),
+                        Center(
+                          child: ElevatedButton(
+                            onPressed: () {
+                              // Tambahkan logika untuk tombol "Add Plan"
+                            },
+                            child: const Text('ADD PLAN'),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ),
+              ],
+            ),
+          ),
+        ),
       ),
     );
   }
